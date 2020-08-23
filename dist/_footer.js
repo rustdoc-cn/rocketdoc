5d66eac48f44c1708a1a38021b796d5240a12147
Ville Zuo <villezuo@163.com>
2020-08-23 16:10:08 +0800

    use markdoc

---
+++ dist/_footer.js
@@ -0,0 +1,5 @@
+const Footer = () => (React.createElement("footer", null,
+    "Powered by",
+    ' ',
+    React.createElement("a", { href: "https://gitee.com/villezuo/markdoc", target: "_blank" }, "Markdoc")));
+export default Footer;
